0ec96451efba436989f9a5ded110e52b29aad46d
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-09-24 00:42:27 +0530

    length of largest possible palindromic string

--- Amazon/LargestPalindromicLength.py
+++ Amazon/LargestPalindromicLength.py
@@ -1,16 +1,27 @@
+"""
+Print the length of largest possible palindromic combination of a substring from a given string.
+
+Input:
+adccccbb
+
+Output:
+7
+
+"""
+
 str = input()
 s = set(str)
-print(s)
+#print(s)
 res = []
 
 for i in s:
     res.append(str.count(i))
     
-print(res)
+#print(res)
 countLength = 0
 for i in res:
     if i%2 == 0:
        countLength += i
        
        
-print(sum+1)
+print(countLength+1)
