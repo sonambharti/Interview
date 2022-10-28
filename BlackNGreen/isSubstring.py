a3b183ce09cf962ce511017eb10399704e1db01d
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-10-28 12:28:40 +0530

    if a string is in other or not

---
+++ BlackNGreen/isSubstring.py
@@ -0,0 +1,30 @@
+def isSubstring(str1, str2):
+    m = len(str1)
+    n = len(str2)
+    
+    for i in range(m-n+1):
+        for j in range(n):
+            if(str1[i+j] != str2[j]):
+                break
+        if j+1 == m:
+            return i
+    return -1
+
+str1 = "Interview"
+str2 = "viewInter"
+
+res = isSubstring(str1, str2)
+
+if res == -1:
+    print("present")
+    
+else:
+    print("not present")
+
+"""
+if str2 in str1:
+    print("present")
+else:
+    print("not present")
+    
+"""
