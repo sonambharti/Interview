2d128a942b0d0f88da487c4b082629425de40b1d
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2023-08-23 01:32:22 +0530

    Update optimal solution

--- Geekathon'21/unleashingArrPotential.py
+++ Geekathon'21/unleashingArrPotential.py
@@ -22,20 +22,36 @@ Geek value = (arr[1]−arr[2])×arr[5] = 63.
 """
 
 def solver(arr, n):
-    res = -2**63-1
     
-    for i in range(n):
-        for j in range(i+1, n):
-            for k in range(j+1, n):
-                mid_res = (arr[i] - arr[j]) * arr[k]
+    # Brute Force approach...
+    
+    # res = -2**63-1
+    
+    # for i in range(n):
+    #     for j in range(i+1, n):
+    #         for k in range(j+1, n):
+    #             mid_res = (arr[i] - arr[j]) * arr[k]
                 
-                res = max(mid_res, res)
+    #             res = max(mid_res, res)
     
 
-        if res < 0:
-            return 0
+    #     if res < 0:
+    #         return 0
+            
+    
+        for i in range(n-2,-1,-1):
+            suff[i] = max(suff[i+1], arr[i+1])
+            
+        prefMax = arr[0]
+        res = 0
+        
+        for j in range(1, n-1):
+           res = max(res, (prefMax - arr[j]) * suff[j]) 
+           prefMax = max(prefMax, arr[j])
+        
             
         return res
+        
 
     
 arr = [9, 3, 2, 5]
