8b26234e94205f381b02ac6518fb11233394c655
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2025-05-28 19:49:00 +0530

    Create Calculate.py

---
+++ LYZR/Calculate.py
@@ -0,0 +1,32 @@
+"""
+#   Calculate(4, 3).add(3).sub(2).div(4).mul(5).get()
+"""
+
+class Calculate:
+    def __init__(self, a, b):
+        self.value = a + b 
+    
+    def add(self, x):
+        self.value += x 
+        return self
+        
+    def sub(self, x):
+        self.value -= x 
+        return self
+        
+    def mul(self, x):
+        self.value *= x 
+        return self
+        
+    def div(self, x):
+        if x==0:
+            raise ValuError("Cannot divided by 0.")
+        self.value /= x 
+        return self
+    def get(self):
+        return self.value 
+        
+if __name__ == "__main__":
+    res = Calculate(4, 3).add(3).sub(2).div(4).mul(5).get()
+    print(f"res = {res}")
+    
